3da80c027e2fdf546a9c61b04e69d1fcee8b1d85
Duane Faria <duanegabrielsouzade.faria@halliburton.com>
2024-10-21 04:44:34 -0300

    feat: add question section to the dns package

--- app/main.py
+++ app/main.py
@@ -1,31 +1,38 @@
 import socket
 
-from app.message import DNSHeader
+from app.message import DNSHeader, DNSQuestion
+
 
 def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
-
+    counter = 0
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
 
-            message = DNSHeader()
-            message.ID = 1234
-            message.QR = 1
-            message.OPCODE = 0
-            message.AA = 0
-            message.TC = 0
-            message.RD = 0
-            message.Z = 0
-            message.RCODE = 0
-            message.QDCOUNT = 0
-            message.ANCOUNT = 0
-            message.NSCOUNT = 0
-            message.ARCOUNT = 0
+            header = DNSHeader()
+            header.ID = 1234
+            header.QR = 1
+            header.OPCODE = 0
+            header.AA = 0
+            header.TC = 0
+            header.RD = 0
+            header.Z = 0
+            header.RCODE = 0
+            header.QDCOUNT = counter
+            header.ANCOUNT = 0
+            header.NSCOUNT = 0
+            header.ARCOUNT = 0
 
-            response = message.convert_into_packet()
+            question = DNSQuestion()
+            question.Name = 'codecrafters.io'
+            question.Type = 1
+            question.Class = 1
 
+            response = header.convert_to_bytes() + question.convert_to_bytes()
+            print(response)
+            counter += 1
             udp_socket.sendto(response, source)
         except Exception as e:
             print(f"Error receiving data: {e}")
